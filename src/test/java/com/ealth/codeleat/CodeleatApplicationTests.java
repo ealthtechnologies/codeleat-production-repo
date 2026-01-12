c67a4b4aaa102b2b07c7dea8810b099d296bae2b
KeyurWarkhedkar <keyurwarkhedkar@gmail.com>
2026-01-12 11:10:14 +0530

    initialize project and folder structure

---
+++ src/test/java/com/ealth/codeleat/CodeleatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ealth.codeleat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeleatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
